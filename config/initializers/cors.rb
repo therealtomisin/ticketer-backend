ca3a9b33c5bb1bc6cd89eb6e5b743e800aa3cccd
therealtomisin <timilehineribake@yahoo.com>
2025-06-30 22:57:43 +0100

    chore: cors correct

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ENV.fetch("FRONTEND_URL"), ENV.fetch("ADMIN_URL_URL")
+    origins ENV.fetch("FRONTEND_URL"), ENV.fetch("ADMIN_URL")
 
     resource "*",
       headers: :any,
